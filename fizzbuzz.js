79bb462b758d8bffe44bae6bf81f71ea170bef70
Keenan Lidral-Porter <klp2012@gmail.com>
2014-11-23 17:24:33 -0800

    adds view model to store information about UI state

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,35 +9,41 @@ fizzbuzz.number = function(){
   };
 };
 
+// vm
+fizzbuzz.vm = new function(){
+  var vm = {};
+  vm.init = function(){
+    vm.number = fizzbuzz.number();
+    vm.display = m.prop(vm.number());
+    vm.numberCheck = function(num){
+      if( num % 3 === 0 && num % 5 === 0){
+        return "FIZZBUZZ";
+      }
+      if( num % 3 === 0 ){
+        return "FIZZ";
+      }
+      if( num % 5 === 0 ){
+        return "BUZZ";
+      }
+      return num;
+    }
+    vm.update = function(){
+      return vm.display(vm.numberCheck(vm.number()));
+    }
+  }
+  return vm;
+};
+
 // controller
 fizzbuzz.controller = function(){
-  var number = fizzbuzz.number();
-  var display = m.prop(number());
-  var numberCheck = function(num){
-    if( num % 3 === 0 && num % 5 === 0){
-      return "FIZZBUZZ";
-    }
-    if( num % 3 === 0 ){
-      return "FIZZ";
-    }
-    if( num % 5 === 0 ){
-      return "BUZZ";
-    }
-    return num;
-  };
-  return {
-    display: display,
-    update: function(){
-      return display(numberCheck(number()));
-    }
-  };
+  fizzbuzz.vm.init();
 };
 
 // view
-fizzbuzz.view = function(ctrl){
+fizzbuzz.view = function(){
   return [
-    m("p", ctrl.display()),
-    m("button", {onclick: ctrl.update}, "+1")
+    m("p", fizzbuzz.vm.display()),
+    m("button", {onclick: fizzbuzz.vm.update}, "+1")
   ];
 };
 
